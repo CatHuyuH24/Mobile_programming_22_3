afbbd4a8d38e9ca99337951973baf7228bee248e
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-10-21 13:25:36 +0700

    add zoom-in when press on thumbnails and "Back" button
    add a new 'SoloImageActivity.java' for the purpose of displaying the image by itself when pressed on (essentially zooming in on that image). The reason a whole new activity is started is that for:
    1. when implementing the "Back" button, only needs to call "finish()" (as can also be seen in the src code)
    2. Inside each zoomed-in frame, there are (potentially) many other features like set as background, next image, previous image, (filter??)
    => Henceforth, it's better to make a whole separate activity rather than make a pop-up layer on top of the main one
    Needed to add the "SoloImageActivity" into the Manifest file, which the AI overlooked; this seriously impeded the progress...

--- app/src/main/java/com/example/mydemoapp/ImageAdapter.java
+++ app/src/main/java/com/example/mydemoapp/ImageAdapter.java
@@ -1,11 +1,19 @@
 package com.example.mydemoapp;
 
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import java.util.List;
@@ -34,6 +42,22 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
         } else {
             Glide.with(context).load(imageItem.getImageId()).into(holder.imageView);
         }
+
+        // Huynh: set onClickListener for the small images (the images displayed initially using the adapters...)
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //change activity to 'SoloImageActivity'
+                Intent soloImageActivityIntent = new Intent(context, SoloImageActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("com.example.mydemoapp.IMAGE_RESOURCE_ID", imageItem.getImageId());
+                bundle.putString("com.example.mydemoapp.IMAGE_URL", imageItem.getImageUrl());
+                soloImageActivityIntent.putExtras(bundle);
+
+                context.startActivity(soloImageActivityIntent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/mydemoapp/SoloImageActivity.java
@@ -0,0 +1,66 @@
+    package com.example.mydemoapp;
+
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
+    import android.widget.Button;
+
+    import androidx.appcompat.app.AppCompatActivity;
+
+    import com.bumptech.glide.Glide;
+
+    public class SoloImageActivity extends AppCompatActivity {
+        private ImageView soloImageView;
+        private TextView tvTitle;
+        private Button backBtn, nextBtn, previousBtn, setBackgroundBtn;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.solo_image); // Create the XML layout
+
+            soloImageView = findViewById(R.id.imgView_solo_image);
+            tvTitle = findViewById(R.id.txtView_solo_image_title);
+            backBtn = findViewById(R.id.btn_solo_back);
+            nextBtn = findViewById(R.id.btn_solo_next);
+            previousBtn = findViewById(R.id.btn_solo_previous);
+
+            // Get the data from the intent
+            Intent intent = getIntent();
+            String imageUrl = intent.getStringExtra("com.example.mydemoapp.IMAGE_URL");
+            int imageId = intent.getIntExtra("com.example.mydemoapp.IMAGE_RESOURCE_ID", -1);
+            String title = "The image id: " + Integer.toString(imageId);
+            tvTitle.setText(title);
+
+            // Load the image using Glide
+            try {
+                if (imageUrl != null) {
+                    Glide.with(this).load(imageUrl).into(soloImageView);
+                } else if (imageId != -1) {
+                    Glide.with(this).load(imageId).into(soloImageView);
+                }
+                else{
+                    //for debugging
+                    //for now, the image URL will always be null (because the current sender is ImageItem)
+                    Toast.makeText(SoloImageActivity.this, "The image ID received is "+imageId+"\nThe image URL received is null", Toast.LENGTH_LONG).show();
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                Toast.makeText(SoloImageActivity.this, "The image ID received is "+imageId+" The image URL received is "+imageUrl, Toast.LENGTH_LONG).show();
+            }
+
+
+            //when pressing "Back" button, go back to the main screen showing thumbnails (small images)
+            //no need to start an intent or activity; this will return to the previous activity on the stack
+            backBtn.setOnClickListener(view -> finish());
+
+
+
+        }
+    }
+
