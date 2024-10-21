dea6d2920dea328c2552a41cc01af014fe23dbe9
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-10-21 17:37:12 +0700

    Add 'set-background' functionality, adjust button to CompatButton, improve buttons' UI
    Changed the UI a bit, making it look nicer by adding colors to the button; still needs more beautification; used AppCompatButton instead of Button for better compatibility with old version (especially this, we're asked to develop with the minSdk = 29 - API = 29 - Android 10.0)
    Added the functionality to set the home screen as the current image, when exiting the application, the home screen still persists. The option to set image as the lock screen may be another button (another functionality rather than included inside the 'set background' functionality).
    After this, need to add animation for transition to the next and previous images ('Next' and 'Previous' buttons).
    About the 'set background' functionality, maybe need to add a loading screen when the image is being processed? And/or allow user to choose angle, position of the image to be set?

--- app/src/main/java/com/example/mydemoapp/SoloImageActivity.java
+++ app/src/main/java/com/example/mydemoapp/SoloImageActivity.java
@@ -1,6 +1,8 @@
     package com.example.mydemoapp;
 
+    import android.app.WallpaperManager;
     import android.content.Intent;
+    import android.graphics.BitmapFactory;
     import android.os.Bundle;
     import android.widget.ImageView;
     import android.widget.TextView;
@@ -26,6 +28,7 @@
             backBtn = findViewById(R.id.btn_solo_back);
             nextBtn = findViewById(R.id.btn_solo_next);
             previousBtn = findViewById(R.id.btn_solo_previous);
+            setBackgroundBtn = findViewById(R.id.btn_solo_set_background);
 
             // Get the data from the intent
             Intent intent = getIntent();
@@ -58,6 +61,33 @@
             //no need to start an intent or activity; this will return to the previous activity on the stack
             backBtn.setOnClickListener(view -> finish());
 
+            //when pressing "Set background button", set the image as the background of the phone's home screen
+            setBackgroundBtn.setOnClickListener(view -> {
+                WallpaperManager wallpaperManger = WallpaperManager.getInstance(getApplicationContext());
+
+                try {
+//                    //set home screen
+                    wallpaperManger.setBitmap(BitmapFactory.decodeResource(getResources(), imageId),null,true,WallpaperManager.FLAG_SYSTEM);
+                    //takes a while to process the image
+                    //not allowing the user to choose the angle, position of the image
+
+                    //TODO: Display a loading screen while the image is being processed? Not sure
+                    //TODO: Allow the user to specify the angle or roation... position of the image about to be set as home screen? Not sure
+
+                    // set lock screen -> MAKE THIS A SEPARATE BUTTON???
+                    // wallpaperManger.setBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.teddy_bg_lock),null,true,WallpaperManager.FLAG_LOCK);
+
+                    //notify the user that the wallpaper has been set
+                    Toast.makeText(SoloImageActivity.this, "Home screen wallpaper has been changed", Toast.LENGTH_LONG).show();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    Toast.makeText(SoloImageActivity.this, "Failed to set wallpaper", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            //TODO: NEXT BUTTON AND PREVIOUS BUTTON WITH ANIMATION
 
 
         }
